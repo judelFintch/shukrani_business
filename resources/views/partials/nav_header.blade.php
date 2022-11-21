d0efc74b5ad91917f7d89d0fb320e78a564d48e6
judel fintch <judfintch@gmail.com>
2022-11-21 10:59:46 +0200

    adding log

--- resources/views/partials/nav_header.blade.php
+++ resources/views/partials/nav_header.blade.php
@@ -1,9 +1,9 @@
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container d-flex align-items-center">
-      <h1 class="logo me-auto"><a href="/">Shukrani Business<span>.</span></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt=""></a>-->
+         <!--<h1 class="logo me-auto"><a href="/">Shukrani Business<span>.</span></a></h1>
+    Uncomment below if you prefer to use an image logo -->
+       <a href="/" class="logo me-auto" ><img src="assets/img/logo1.jpg" alt=""></a>
 
       <nav id="navbar" class="navbar order-last order-lg-0">
         <ul>
